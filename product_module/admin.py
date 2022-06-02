7dad71e505ab25cfd91b1db55d6de62521a540bc
ashna <ashnam.tamang@apexcollege.edu.np>
2022-06-02 07:20:00 +0545

    changes

--- product_module/admin.py
+++ product_module/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Brand
-admin.site.register(Brand)
+from .models import Brand, Category, Product
+admin.site.register(Category)
+
+
+class BrandAdmin(admin.ModelAdmin):
+ list_display = ["name" , "is_active",]
+ search_fields = ["name","is_active",]
+ list_filter = ["name","is_active",]
+class Meta:
+    model = Brand
+admin.site.register(Brand, BrandAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+ list_display = ["name" , "is_active",]
+ search_fields = ["name","is_active",]
+ list_filter = ["name","is_active",]
+ 
+class Meta:
+    model = Category
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "brand", "category",]
+    search_fields = ["name", "price", "brand__name", "category__name",]
+    list_filter = ["brand","category",]
+    readonly_fields = ["quantity",]
+
+class Meta:
+    model = Product
+
+admin.site.register(Product, ProductAdmin)
